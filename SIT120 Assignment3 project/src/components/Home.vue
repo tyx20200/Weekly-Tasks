38f258e5b27fed76259c23fe6830ef5e8c21ab28
Mark <113773757+tyx20200@users.noreply.github.com>
2022-10-24 23:52:35 +1100

    SIT120 A3

---
+++ SIT120 Assignment3 project/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <main class="main">
+    <section class="sec">
+        <div class="titleimg">
+            <div class="title">
+           Adventure in  Teyvat<br>
+               
+            </div>
+        </div>
+    </section>
+    <section class="sec">
+        
+            <div class="part1 pt">
+                <div class="imghid"><div class="img"></div></div>
+                <div class="txt">
+                    <h1>Development</h1>
+                    <p>Genshin Impact began development as early as late January 2017, with an initial team of 120 people, which grew to 400 by the end of that year, and reached 700 by February 2021. miHoYo revealed the game in June 2019 at E3 2019.</p>
+                    
+                </div>
+            </div>
+     
+            <div class="part2 pt">
+                <div class="txt">
+                    <h1>Music</h1>
+                    <p>Yu-Peng Chen of HOYO-MiX composed the game's original score, which was performed by the London Philharmonic Orchestra, the Shanghai Symphony Orchestra, and the Tokyo Philharmonic Orchestra.</p>
+                    
+                </div>
+                <div class="imghid"><div class="img"></div></div>
+            </div>
+
+      
+            <div class="part3 pt">
+                <div class="imghid"><div class="img"></div></div>
+                <div class="txt">
+                    <h1>Release</h1>
+                    <p>The worldwide release, September 28, was announced on August 17 for PC and mobile platforms, and on August 28 it revealed that the PlayStation 4 version would be releasing on the same date.</p>
+                    
+                </div>
+            </div>
+       
+     
+            <div class="part4 pt">
+                <div class="txt">
+                    <h1>Awards</h1>
+                    <p>App Store: "iPhone Game of the Year" in the App Store Best of 2020, 2021 Apple Design Awards for Visuals and Graphics, Google Play: "Best Game of 2020"</p>
+                    
+                </div>
+                <div class="imghid"><div class="img"></div></div>
+            </div>
+  
+    </section>
+       
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+name:"Home",
+}
+</script>
+
+<style>
+
+</style>
